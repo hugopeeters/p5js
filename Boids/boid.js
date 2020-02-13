8f1852cacb054bd4f7a4143a674c239451f2dd89
hugopeeters <hpeeters@me.com>
2020-02-13 19:50:53 +0100

    created framework for three forces

--- Boids/boid.js
+++ Boids/boid.js
@@ -2,48 +2,89 @@ class Boid {
 
     constructor(x, y) {
         this.pos = createVector(x, y);
-        this.heading = p5.Vector.random2D();
-        this.vel = random(1, 2);
-        this.heading.mult(this.vel);
+        this.vel = p5.Vector.random2D().mult(random(1, 2));
+        this.acc = createVector(0, 0);
+        this.alpha = PI / 8;
+        this.d1 = 12;
+        this.d2 = 0.8 * this.d1;
     }
 
     render() {
         noFill();
         stroke(255);
-        let alpha = PI / 8;
-        let d1 = 12;
-        let d2 = 10;
-
         let x1 = 0;
         let y1 = 0;
-        let x2 = x1 - d1 * cos(alpha);
-        let y2 = y1 - d1 * sin(alpha);
-        let x3 = x1 - d2;
+        let x2 = x1 - this.d1 * cos(this.alpha);
+        let y2 = y1 - this.d1 * sin(this.alpha);
+        let x3 = x1 - this.d2;
         let y3 = y1;
-        let x4 = x1 - d1 * cos(alpha);
-        let y4 = y1 + d1 * sin(alpha);
+        let x4 = x1 - this.d1 * cos(this.alpha);
+        let y4 = y1 + this.d1 * sin(this.alpha);
         push();
         translate(this.pos.x, this.pos.y);
-        rotate(this.heading.heading());
+        rotate(this.vel.heading());
         quad(x1, y1, x2, y2, x3, y3, x4, y4);
         pop();
     }
 
-    update(){
-        this.pos.add(this.heading);
-        if (this.pos.x > width){
-            this.pos.x -= width;
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    update() {
+
+        let steeringForce = createVector();
+        //separation
+        let separationForce = this.steerAwayFromCloseBoids(boids);
+        steeringForce.add(separationForce);
+        //alignment
+        let alignmentForce = this.alignWithCloseBoids(boids);
+        steeringForce.add(alignmentForce);
+        //cohesion
+        let cohesionForce = this.steerTowardsLocalCenterOfMass(boids);
+        steeringForce.add(cohesionForce);
+        //apply the combined forces
+        this.applyForce(steeringForce);
+
+        //calculate new velocity and position
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        
+        // loop around the edges
+        if (this.pos.x > width + this.d1) {
+            this.pos.x = 0 - this.d1;
         }
-        if (this.pos.x < 0){
-            this.pos.x += width;
+        if (this.pos.x < 0 - this.d1) {
+            this.pos.x = width + this.d1;
         }
-        if (this.pos.y > height){
-            this.pos.y -= height;
+        if (this.pos.y > height + this.d1) {
+            this.pos.y = 0 - this.d1;
         }
-        if (this.pos.y < 0){
-            this.pos.y += height;
+        if (this.pos.y < 0 - this.d1) {
+            this.pos.y = height + this.d1;
         }
         
+        //reset acceleration
+        this.acc.setMag(0);
+    }
+
+    steerAwayFromCloseBoids(others) {
+        let tooClose = 25;
+
+        let force = createVector();
+        return force;
+    }
+
+    alignWithCloseBoids(others){
+
+        let force = createVector();
+        return force;
+    }
+
+    steerTowardsLocalCenterOfMass(others){
+
+        let force = createVector();
+        return force;
     }
 
 }
